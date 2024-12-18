6be5a1be9db56c1a2b4d678c458e61f00938d968
amirmtin <amirmatinrousta@gmail.com>
2024-12-18 20:55:43 +0330

    added NqueensSolver with initial methods

--- src/nqueens.rs
+++ src/nqueens.rs
@@ -22,3 +22,53 @@ impl Individual {
         self.fitness = non_attacking_pairs;
     }
 }
+
+pub struct NQueensSolver {
+    pub n: usize,
+    max_fitness: usize,
+    pub population: Vec<Individual>,
+    population_size: usize,
+    mutation_rate: f64,
+    generation_limit: usize,
+    rng: ThreadRng,
+}
+
+impl NQueensSolver {
+    pub fn new(
+        n: usize,
+        population_size: usize,
+        mutation_rate: f64,
+        generation_limit: usize,
+    ) -> Self {
+        let max_fitness = n * (n - 1) / 2;
+        let mut rng = rand::thread_rng();
+        let population = Self::initial_population(n, &mut rng);
+        Self {
+            n,
+            max_fitness,
+            population,
+            population_size,
+            mutation_rate,
+            generation_limit,
+            rng,
+        }
+    }
+
+    fn initial_population(n: usize, rng: &mut ThreadRng) -> Vec<Individual> {
+        let mut population = Vec::new();
+        for _ in 0..n {
+            let mut genes = Vec::new();
+            for _ in 0..n {
+                genes.push(Self::gen_rand(n, rng));
+            }
+            let mut individual = Individual { genes, fitness: 0 };
+            individual.calculate_fitness();
+            population.push(individual);
+        }
+        population
+    }
+
+    fn gen_rand(n: usize, rng: &mut ThreadRng) -> usize {
+        rng.gen_range(0..n)
+    }
+}
