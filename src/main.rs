25502e70506c99009f0835bf89149bf508ba547c
amirmtin <amirmatinrousta@gmail.com>
2024-12-19 18:15:28 +0330

    added print_chess_board for pretty printing the solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+mod nqueens;
+
+fn print_chess_board(solution: &nqueens::Individual, n: usize) {
+    println!("Solution:");
+    for row in 0..n {
+        for col in 0..n {
+            if solution.genes[row] == col {
+                print!("♛  ");
+            } else {
+                print!(".  ");
+            }
+        }
+        println!("\n");
+    }
+    println!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 8;
+    let population_size = 1000;
+    let mutation_rate = 0.1;
+    let generation_limit = 5000;
+
+    let mut solver = nqueens::NQueensSolver::new(n, population_size, mutation_rate, generation_limit);
+
+    match solver.run() {
+        Some((solution, generation)) => {
+            println!("Solution found in generation {}: {:?}", generation, solution.genes);
+            print_chess_board(&solution, n);
+        }
+        None => {
+            println!("No solution found within the generation limit.");
+        }
+    }
 }
