e371fc5c36a7dde6572ecb73460b49016d601828
amirmtin <amirmatinrousta@gmail.com>
2024-12-19 18:15:05 +0330

    use Arc for poining to population

--- src/nqueens.rs
+++ src/nqueens.rs
@@ -1,6 +1,6 @@
 use rand::prelude::*;
 use rayon::prelude::*;
-use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::{Arc, mpsc::{self, Receiver, Sender}};
 use std::thread;
 
 #[derive(Debug, Clone)]
@@ -56,17 +56,17 @@ impl NQueensSolver {
     pub fn run(&mut self) -> Option<(Individual, usize)> {
         println!("Max fitness: {}", self.max_fitness);
 
-        let mut population = Self::initial_population(self.n, self.population_size);
+        let mut population = Arc::new(Self::initial_population(self.n, self.population_size));
         let n = self.n;
         let mutation_rate = self.mutation_rate;
 
         for generation in 0..self.generation_limit {
             let (select_tx, select_rx) = mpsc::channel();
 
-            let population_clone = population.clone();
+            let population_clone = Arc::clone(&population);
 
             thread::spawn(move || {
-                Self::select_step(&population_clone, select_tx);
+                Self::select_step(population_clone, select_tx);
             });
 
             let (crossover_tx, crossover_rx) = mpsc::channel();
@@ -95,7 +95,7 @@ impl NQueensSolver {
             }
 
             println!("Generation {}: Best fitness: {}", generation, best_fitness);
-            population = new_population;
+            population = Arc::new(new_population);
         }
         None
     }
@@ -112,23 +112,15 @@ impl NQueensSolver {
         &population[0]
     }
 
-    fn select_step(population: &Vec<Individual>, tx: Sender<(Individual, Individual)>) {
-        let total_fitness: usize = Self::sum_fitness(population);
+    fn select_step(population: Arc<Vec<Individual>>, tx: Sender<(Individual, Individual)>) {
+        let total_fitness: usize = Self::sum_fitness(&population);
         let population_size = population.len();
-        // for _ in (0..population_size).step_by(2) {
-        //     let pop1 = Self::select_individual(&population, total_fitness);
-        //     let pop2 = Self::select_individual(&population, total_fitness);
-        //     match tx.send((pop1.clone(), pop2.clone())) {
-        //         Result::Ok(_) => {},
-        //         Result::Err(_) => {},
-        //     }
-        // }
 
         (0..population_size)
             .into_par_iter()
             .step_by(2)
             .for_each(|_| {
-                let population_clone = population.clone();
+                let population_clone = Arc::clone(&population);
                 let tx_clone = tx.clone();
 
                 rayon::spawn(move || {
@@ -143,27 +135,28 @@ impl NQueensSolver {
     }
 
     fn crossover_step(n: usize, rx: Receiver<(Individual, Individual)>, tx: Sender<Individual>) {
-        for (parent1, parent2) in rx {
+        let parents: Vec<(Individual, Individual)> = rx.iter().collect();
+
+        parents.into_par_iter().for_each(|(parent1, parent2)| {
             let (child1, child2) = Self::crossover(n, &parent1, &parent2);
-            match tx.send(child1) {
-                Result::Ok(_) => {}
-                Result::Err(_) => {}
+            if let Err(e) = tx.send(child1) {
+                eprintln!("Failed to send child1: {}", e);
             }
-            match tx.send(child2) {
-                Result::Ok(_) => {}
-                Result::Err(_) => {}
+            if let Err(e) = tx.send(child2) {
+                eprintln!("Failed to send child2: {}", e);
             }
-        }
+        });
     }
 
     fn mutate_step(rx: Receiver<Individual>, tx: Sender<Individual>, n: usize, mutation_rate: f64) {
-        for mut individual in rx {
+        let individuals: Vec<Individual> = rx.iter().collect();
+
+        individuals.into_par_iter().for_each(|mut individual| {
             Self::mutate(&mut individual, n, mutation_rate);
-            match tx.send(individual) {
-                Result::Ok(_) => {}
-                Result::Err(_) => {}
+            if let Err(e) = tx.send(individual) {
+                eprintln!("Failed to send mutated individual: {}", e);
             }
-        }
+        });
     }
 
     fn initial_population(n: usize, population_size: usize) -> Vec<Individual> {
